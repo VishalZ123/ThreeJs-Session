8b43d0b1e43d0e56f133a7b60d1af5dcc5e0468d
Vishal <yadav.40@iitj.ac.in>
2022-07-24 04:43:23 +0530

    Adding controls and use of Anti-aliasing

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color("rgb(20,20,20)");
@@ -6,7 +7,7 @@ scene.background = new THREE.Color("rgb(20,20,20)");
 const app = document.querySelector("#app");
 
 //renderer
-const renderer = new THREE.WebGLRenderer(); // WebGL Renderer
+const renderer = new THREE.WebGLRenderer({ antialias: true }); // WebGL Renderer
 renderer.setPixelRatio(window.devicePixelRatio); // set device pixel ratio
 renderer.setSize(app.offsetWidth, app.offsetHeight); // Resizes the output canvas to given width and height
 app.appendChild(renderer.domElement);
@@ -14,7 +15,7 @@ app.appendChild(renderer.domElement);
 //camera
 const camera = new THREE.PerspectiveCamera(
   60, // fov
-  app.offsetWidth/app.offsetHeight, // aspect ratio
+  app.offsetWidth / app.offsetHeight, // aspect ratio
   0.1, // near
   1200 // far
 );
@@ -22,14 +23,18 @@ camera.position.set(0, 0, 5);
 
 // cube
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({color:0x00ff00});
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
+// orbit controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+
 //animate and render
-renderer.render(scene, camera);
-// function animate() {
-//   requestAnimationFrame(animate);
-//   controls.update();
-// }
-// animate();
+function animate() {
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+  controls.update();
+}
+animate();
