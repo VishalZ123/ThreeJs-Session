9f83ef725c33d7281c2aaa3b34664e2c9d1069d0
Vishal <yadav.40@iitj.ac.in>
2022-07-24 04:31:51 +0530

    Makes a cube using box geometry and green material

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 import * as THREE from "three";
+
 const scene = new THREE.Scene();
 scene.background = new THREE.Color("rgb(20,20,20)");
 
 const app = document.querySelector("#app");
 
 //renderer
-const renderer = new THREE.WebGLRenderer({ antialias: true }); // WebGL Renderer
+const renderer = new THREE.WebGLRenderer(); // WebGL Renderer
 renderer.setPixelRatio(window.devicePixelRatio); // set device pixel ratio
 renderer.setSize(app.offsetWidth, app.offsetHeight); // Resizes the output canvas to given width and height
 app.appendChild(renderer.domElement);
@@ -18,6 +19,12 @@ const camera = new THREE.PerspectiveCamera(
   1200 // far
 );
 camera.position.set(0, 0, 5);
+
+// cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({color:0x00ff00});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
 //animate and render
 renderer.render(scene, camera);
