9ae2e57273a619039a9c73382bac96c4ccaca044
Vishal <yadav.40@iitj.ac.in>
2022-07-23 18:12:58 +0530

    Adds camera and Renderer

--- main.js
+++ main.js
@@ -2,24 +2,27 @@ import * as THREE from "three";
 const scene = new THREE.Scene();
 scene.background = new THREE.Color("rgb(20,20,20)");
 
-//camera
-const camera = new THREE.PerspectiveCamera(
-  60,
-  innerWidth / innerHeight,
-  0.1,
-  1200
-);
+const app = document.querySelector("#app");
 
 //renderer
-const app = document.querySelector("#app");
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(app.offsetWidth, app.offsetHeight);
 app.appendChild(renderer.domElement);
 
+//camera
+const camera = new THREE.PerspectiveCamera(
+  60,
+  app.offsetWidth/app.offsetHeight,
+  0.1,
+  1200
+);
+camera.position.set(0, 0, 5);
+
 //animate and render
 renderer.render(scene, camera);
 // function animate() {
 //   requestAnimationFrame(animate);
+//   controls.update();
 // }
 // animate();
