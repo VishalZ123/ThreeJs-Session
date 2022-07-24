f5e6f52264c43490ac16eb6de64509f0099e5327
Vishal <yadav.40@iitj.ac.in>
2022-07-24 06:18:30 +0530

    Materials

--- main.js
+++ main.js
@@ -19,30 +19,23 @@ const camera = new THREE.PerspectiveCamera(
   0.1, // near
   1200 // far
 );
-camera.position.set(0, 2, 5);
+camera.position.set(0, 2, 50);
 
-// cube
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshStandardMaterial({color: 0xc0c0c0});
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+const geometry = new THREE.TorusKnotGeometry( 10, 3, 100, 16 );
+// const material = new THREE.MeshBasicMaterial({ color: 0x00ffff });
+const material = new THREE.MeshPhongMaterial({ color: 0x00ffff});
+const material2 = new THREE.MeshLambertMaterial({ color: 0x00ffff});
+const mesh1 = new THREE.Mesh(geometry, material);
+const mesh2 = new THREE.Mesh(geometry, material2);
+mesh1.position.x = -20;
+mesh2.position.x = 20;
+
+scene.add(mesh1, mesh2);
 
 // lights
-// const light = new THREE.AmbientLight({color: 0x000});
-// const light = new THREE.DirectionalLight({color: 0xfff});
-// scene.add(light);
-
-const light1 = new THREE.DirectionalLight({color:0xffffff});
-const helper1 = new THREE.DirectionalLightHelper( light1, 5, true );
-light1.position.set(2, 2, 4);
-
-const light2 = new THREE.DirectionalLight({color: 0xffffff});
-const helper2 = new THREE.DirectionalLightHelper( light2, 5, true );
-light2.position.set(-2, -2, -4);
-
-scene.add(light1, light2, helper1, helper2);
-
-// light1.lookAt(new THREE.Vector3(0,0,0));
+const light = new THREE.DirectionalLight({color: 0x000});
+light.position.set(0,0,10);
+scene.add(light);
 
 // orbit controls
 const controls = new OrbitControls(camera, renderer.domElement);
