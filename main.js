8aab82f59921a875da319c3c15a054024b5925d0
Vishal <yadav.40@iitj.ac.in>
2022-07-24 06:11:49 +0530

    Adds lights and light helpers

--- main.js
+++ main.js
@@ -19,13 +19,30 @@ const camera = new THREE.PerspectiveCamera(
   0.1, // near
   1200 // far
 );
-camera.position.set(0, 0, 5);
+camera.position.set(0, 2, 5);
 
 // cube
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const material = new THREE.MeshStandardMaterial({color: 0xc0c0c0});
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
+
+// lights
+// const light = new THREE.AmbientLight({color: 0x000});
+// const light = new THREE.DirectionalLight({color: 0xfff});
+// scene.add(light);
+
+const light1 = new THREE.DirectionalLight({color:0xffffff});
+const helper1 = new THREE.DirectionalLightHelper( light1, 5, true );
+light1.position.set(2, 2, 4);
+
+const light2 = new THREE.DirectionalLight({color: 0xffffff});
+const helper2 = new THREE.DirectionalLightHelper( light2, 5, true );
+light2.position.set(-2, -2, -4);
+
+scene.add(light1, light2, helper1, helper2);
+
+// light1.lookAt(new THREE.Vector3(0,0,0));
 
 // orbit controls
 const controls = new OrbitControls(camera, renderer.domElement);
